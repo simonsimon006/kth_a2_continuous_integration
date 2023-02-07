4004abae3e709f689675727e00df1b118f3e572d
Simon Grunwald <simongru@student.ethz.ch>
2023-02-07 15:32:53 +0100

    feat: Adds compilation on a fixed path, part of #6 .

--- app/src/main/java/kth_a2_continuous_integration/ContinuousIntegrationServer.java
+++ app/src/main/java/kth_a2_continuous_integration/ContinuousIntegrationServer.java
@@ -1,53 +1,62 @@
 package kth_a2_continuous_integration;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
- 
-import java.io.IOException;
- 
-import org.eclipse.jetty.server.Server;
+
 import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.gradle.tooling.*;
 
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.api.errors.InvalidRemoteException;
-import org.eclipse.jgit.api.errors.TransportException;
+/**
+ * Skeleton of a ContinuousIntegrationServer which acts as webhook
+ * See the Jetty documentation for API documentation of those classes.
+ */
+public class ContinuousIntegrationServer extends AbstractHandler {
+	public void handle(String target,
+			Request baseRequest,
+			HttpServletRequest request,
+			HttpServletResponse response)
+			throws IOException, ServletException {
+		response.setContentType("text/html;charset=utf-8");
+		response.setStatus(HttpServletResponse.SC_OK);
+		baseRequest.setHandled(true);
 
-/** 
- Skeleton of a ContinuousIntegrationServer which acts as webhook
- See the Jetty documentation for API documentation of those classes.
-*/
-public class ContinuousIntegrationServer extends AbstractHandler
-{
-    public void handle(String target,
-                       Request baseRequest,
-                       HttpServletRequest request,
-                       HttpServletResponse response) 
-        throws IOException, ServletException
-    {
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-        baseRequest.setHandled(true);
+		System.out.println(target);
 
-        System.out.println(target);
+		// here you do all the continuous integration tasks
+		// for example
+		// 1st clone your repository
+		// 2nd compile the code
+		ProjectConnection connection = GradleConnector.newConnector()
+				.forProjectDirectory(new File(
+						"/home/simon/Code/DECIDE/"))
+				.connect();
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
 
-        // here you do all the continuous integration tasks
-        // for example
-        // 1st clone your repository
-        // 2nd compile the code
+		try {
 
-        response.getWriter().println("CI job done");
-    }
- 
-    // used to start the CI server in command line
-    public static void main(String[] args) throws Exception
-    {
+			connection.newBuild().forTasks("build").setStandardOutput(output).run();
+		} catch (Exception e) {
+			response.getWriter().println(e.getMessage());
+		} finally {
+			connection.close();
+		}
+		response.getWriter().println(output.toString());
+		output.close();
+	}
 
-        Server server = new Server(8080);
-        server.setHandler(new ContinuousIntegrationServer()); 
-        server.start();
-        server.join();
-    }
+	// used to start the CI server in command line
+	public static void main(String[] args) throws Exception {
+
+		Server server = new Server(8080);
+		server.setHandler(new ContinuousIntegrationServer());
+		server.start();
+		server.join();
+	}
 }

ed4ac4083b3f6d1c6f851163c555950e09232202
JesperSlagarp <61503896+JesperSlagarp@users.noreply.github.com>
2023-02-07 15:38:40 +0100

    doc: Added documentation comment to CommandLine.exec (isssue #5 )

--- app/src/main/java/kth_a2_continuous_integration/CommandLine.java
+++ app/src/main/java/kth_a2_continuous_integration/CommandLine.java
@@ -7,6 +7,15 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class CommandLine {
+
+    /**
+     * The function executes a console/terminal command and returns the text output as a string.
+     * Designed to work on unix based and windows systems.
+     * @param command
+     * @param path
+     * @return String: Console output
+     * @throws IOException
+     */
     public static String exec(String command, String path) throws IOException {
         Process process;
         String os = System.getProperty("os.name").toLowerCase();

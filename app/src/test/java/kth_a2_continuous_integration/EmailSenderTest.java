e2fb2ce2426742a00de6387461d4b1ac13088d3c
Oscar Olsson <oscols@kth.se>
2023-02-08 14:06:27 +0100

    feat: Added negative test for sendEmail() (issue #8)

--- app/src/test/java/kth_a2_continuous_integration/EmailSenderTest.java
+++ app/src/test/java/kth_a2_continuous_integration/EmailSenderTest.java
@@ -28,4 +28,19 @@ public class EmailSenderTest {
         String text = "TESTING TESTING";
         assertTrue(emailSender.sendEmail(to, from, subject, text), "sendEmail did not return true when it should");
     }
+
+    /**
+	 * Tests sendEmail to return false when mail and password is correct
+     * but the recipient does NOT exits.
+	 * 
+	 * @return Needs to evaluate to false.
+	 */
+    @Test
+    void sendEmailNegativeTest() {
+        String to = "dd2480aaaaaa@gmail.com";
+        String from = "dd2480group19@gmail.com";
+        String subject = "Sending email";
+        String text = "TESTING TESTING";
+        assertTrue(emailSender.sendEmail(to, from, subject, text), "sendEmail did not return false when it should");
+    }
 }

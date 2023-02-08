606a90f02415c634f2acb421c62689ab160c219c
Linnéa Gustafsson <linnea.gustafsson@hotmail.se>
2023-02-08 22:34:35 +0100

    doc: Fixed line length excess

--- app/src/main/java/kth_a2_continuous_integration/CommandLine.java
+++ app/src/main/java/kth_a2_continuous_integration/CommandLine.java
@@ -25,8 +25,16 @@ public class CommandLine {
     public static String exec(String command, String path) throws IOException {
         Process process;
         String os = System.getProperty("os.name").toLowerCase();
-        if("windows 10".equals(os) || "windows 11".equals(os)) process = Runtime.getRuntime().exec(new String[]{"cmd.exe", "/c", command}, null, new File(path));
-        else                                                   process = Runtime.getRuntime().exec(new String[]{"bin/sh", "-c", command}, null, new File(path));
+        if("windows 10".equals(os) || "windows 11".equals(os)) {
+            process = Runtime.getRuntime().exec(
+                    new String[]{"cmd.exe", "/c", command}, null, new File(path)
+                );
+        }
+        else {
+            process = Runtime.getRuntime().exec(
+                    new String[]{"bin/sh", "-c", command}, null, new File(path)
+                );
+        }
 
         return output(process.getInputStream());
     }

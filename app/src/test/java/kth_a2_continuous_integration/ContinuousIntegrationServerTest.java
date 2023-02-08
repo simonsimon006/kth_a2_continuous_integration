776733fd5b0894f455e9c62ad763315fde4012c7
Simon Grunwald <simongrunwald@hotmail.de>
2023-02-08 10:44:33 +0100

    feat: Adds a dummy test for execute. Preparation for #6 and #7 .

--- app/src/test/java/kth_a2_continuous_integration/ContinuousIntegrationServerTest.java
+++ app/src/test/java/kth_a2_continuous_integration/ContinuousIntegrationServerTest.java
@@ -3,11 +3,28 @@ package kth_a2_continuous_integration;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
 public class ContinuousIntegrationServerTest {
     /*
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+     * @Test void appHasAGreeting() {
+     * App classUnderTest = new App();
+     * assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+     * }
+     */
+    @BeforeEach
+    void setUp() {
     }
-    */
+
+    /**
+     * Tests execute() by building the DECIDE project as a sample project.
+     *
+     * @return Needs to evaluate to true.
+     */
+    @Test
+    @DisplayName("Sample project is built successfully")
+    void buildSampleProject() {
+        assertTrue(true, "Dummy test.");
+    }
 }
